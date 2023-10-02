6fdf60abf6adb988609d034f81d2cee2a3b056f9
andreitotoro <andreitotoro@gmail.com>
2023-10-02 15:48:50 +0300

    Facut regulament sa scaleze

--- pages/regulament.tsx
+++ pages/regulament.tsx
@@ -1,29 +1,29 @@
 import Layout from "@/ components/Layout";
-import { Box, Center, Divider, Stack, Text } from "@chakra-ui/react";
+import { Box, Center, Divider, Stack, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
 export default function Regulament() {
   return (
     <Layout>
-      <Center>
-        <Stack>
-          <Text
-            color={"black"}
-            fontSize={"4xl"}
-            fontWeight={"bold"}
-          >
-            Regulament
-          </Text>
-          <Divider />
+      <Stack>
+        <Text
+          color={"black"}
+          fontSize={"4xl"}
+          fontWeight={"bold"}
+        >
+          Regulament
+        </Text>
+        <Divider />
+        <VStack>
           <iframe
-            width={"1200px"}
+            width={"100%"}
             height={"600px"}
             src={
               "https://creangaprinstiinte.files.wordpress.com/2022/07/regulament-creanga-prin-stiinte-2022_220711_164437.pdf"
             }
           />
-        </Stack>
-      </Center>
+        </VStack>
+      </Stack>
     </Layout>
   );
 }

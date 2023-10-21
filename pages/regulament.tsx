f8f624817f6fadea64bc15e78fa406df7a648f14
andreitotoro <andreitotoro@gmail.com>
2023-10-21 12:08:51 +0300

    Adaugat dropdown sectiune regulament

--- pages/regulament.tsx
+++ pages/regulament.tsx
@@ -1,3 +1,4 @@
+import Dropdown from "@/ components/Dropdown";
 import Layout from "@/ components/Layout";
 import { Box, Center, Divider, Stack, Text, VStack } from "@chakra-ui/react";
 import React from "react";
@@ -5,25 +6,27 @@ import React from "react";
 export default function Regulament() {
   return (
     <Layout>
-      <Stack>
-        <Text
-          color={"white"}
-          fontSize={"4xl"}
-          fontWeight={"bold"}
-        >
-          Regulament
-        </Text>
-        <Divider />
-        <VStack>
-          <iframe
-            width={"100%"}
-            height={"600px"}
-            src={
-              "https://creangaprinstiinte.files.wordpress.com/2023/04/regulament-creangaprinstiinte2023.pdf"
-            }
-          />
-        </VStack>
-      </Stack>
+      <VStack
+        p={[2, 2, 2, 10]}
+        bg={"#fb4f0e"}
+        gap={5}
+        w={["100%", "100%", "100%", "70%"]}
+        flexDir={"column"}
+        justify={"center"}
+        align={"center"}
+        justifyContent={"center"}
+        mx={"auto"}
+      >
+        <Text fontSize={"3xl"}>Regulament</Text>
+        <Dropdown
+          dropdownTitle="Regulament Creanga Prin Stiinte 2023"
+          dropdownFile="https://creangaprinstiinte.files.wordpress.com/2023/04/regulament-creangaprinstiinte2023.pdf"
+        />
+        <Dropdown
+          dropdownTitle="Regulament Creanga Prin Stiinte 2022"
+          dropdownFile="https://creangaprinstiinte.files.wordpress.com/2022/07/regulament-creanga-prin-stiinte-2022_220711_164437.pdf"
+        />
+      </VStack>
     </Layout>
   );
 }
